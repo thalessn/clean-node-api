719b9346ec78a7003ce1098425625c475fae7a38
Thales Nascimento <thales.silva.nascimento@gmail.com>
2021-11-01 14:01:16 -0300

    test: ensure EmailValidatorAdapter returns false if validator returns false

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -244,4 +244,22 @@ describe('SignUp Controller', () => {
       password: 'valid_password'
     })
   })
+
+  test('Shoulkd call EmailValidator with correct email', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    await sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
 })
